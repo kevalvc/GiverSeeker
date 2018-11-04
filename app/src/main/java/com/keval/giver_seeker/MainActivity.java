05b25704f3014cfaec8717e1897630ac0e76d01b
kevalvc <chavda.keval97@gmail.com>
2018-11-04 16:46:12 +0530

    Basic Firebase Connections

--- app/src/main/java/com/keval/giver_seeker/MainActivity.java
+++ app/src/main/java/com/keval/giver_seeker/MainActivity.java
@@ -2,12 +2,43 @@ package com.keval.giver_seeker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+//            ((TextView) findViewById(R.id.textSignInStatus)).setText(
+//                    "User ID: " + user.getUid());
+            Toast.makeText(this, "User ID: " + user.getUid(), Toast.LENGTH_SHORT).show();
+        } else {
+//            ((TextView) findViewById(R.id.textSignInStatus)).setText(
+//                    "Error: sign in failed.");
+            Toast.makeText(this, "Error: sign in failed.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
